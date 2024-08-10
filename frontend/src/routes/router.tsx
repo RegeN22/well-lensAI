db3cb4b3e3a073f52704059cf56bd30d04624222
Noam Ben Zeev <noambz100@gmail.com>
2024-08-10 16:46:05 +0300

    save

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -1,14 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
-import ScansPage from "./ScansPage/ScansPage";
 import NewScanPage from "./NewScanPage/NewScanPage";
+import ScansPage from "./ScansPage/ScansPage";
+import SignIn from "./sign-in/sign-in";
+import SignUp from "./sign-up/sign-up";
 
 export const appRouter = createBrowserRouter([
   {
-    path: '/',
-    element: <ScansPage />
+    path: "/",
+    element: <SignIn />,
   },
   {
-    path: '/new',
-    element: <NewScanPage />
-  }
+    path: "/sign-up",
+    element: <SignUp />,
+  },
+  {
+    path: "/home",
+    element: <ScansPage />,
+  },
+  {
+    path: "/new",
+    element: <NewScanPage />,
+  },
 ]);
