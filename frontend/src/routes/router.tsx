ad52057b9aac13a6b64295c71c837a356f6ca45c
Noam Ben Zeev <noambz100@gmail.com>
2024-08-10 14:56:52 +0300

    remove index

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -1,15 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import ScansPage from "./ScansPage/ScansPage";
 import NewScanPage from "./NewScanPage/NewScanPage";
-import Index from ".";
 
 export const appRouter = createBrowserRouter([
   {
     path: '/',
-    element: <Index />
-  },
-  {
-    path: '/home',
     element: <ScansPage />
   },
   {
