fdbecca8d8a14e7eec77e34ebbd4d72ef6171add
Noam Ben Zeev <noambz100@gmail.com>
2024-08-24 17:35:24 +0300

    fix logout

--- frontend/src/routes/ProfilePage/ProfilePage.tsx
+++ frontend/src/routes/ProfilePage/ProfilePage.tsx
@@ -1,27 +1,62 @@
-import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Paper, Stack, Typography } from "@mui/material";
-import UserSummaryCard from '../../features/user-profile/UserSummaryCard/UserSummaryCard';
+import LogoutIcon from "@mui/icons-material/Logout";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import UserSummaryCard from "../../features/user-profile/UserSummaryCard/UserSummaryCard";
 import { useCurrentUser } from "../../hooks/user/useCurrentUser";
-import LogoutIcon from '@mui/icons-material/Logout';
 import { logout } from "../../services/user-service";
 
 export default function ProfilePage(): JSX.Element {
+  const navigate = useNavigate();
   const [profile] = useCurrentUser();
-  return <Stack sx={{ padding: '1em' }} spacing={2}>
-    <Box sx={{textAlign: 'center'}}>
-      <Typography variant="h4">Profile</Typography>
-    </Box>
-    <UserSummaryCard user={profile} />
-    <Paper>
-      <List>
-        <ListItem disablePadding>
-          <ListItemButton onClick={() => logout()}>
-            <ListItemIcon>
-              <LogoutIcon />
-            </ListItemIcon>
-            <ListItemText primary="Log Out" />
-          </ListItemButton>
-        </ListItem>
-      </List>
-    </Paper>
-  </Stack>;
+
+  useEffect(() => {
+    const currentUser = localStorage.getItem("currentUser");
+    if (!currentUser) {
+      navigate("/");
+    }
+  }, [navigate]);
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate("/");
+    } catch (error) {
+      alert(
+        (error as { response: { data: { message: string } } })?.response?.data
+          ?.message ?? "Something went wrong"
+      );
+    }
+  };
+
+  return (
+    <Stack sx={{ padding: "1em" }} spacing={2}>
+      <Box sx={{ textAlign: "center" }}>
+        <Typography variant="h4">Profile</Typography>
+      </Box>
+      <UserSummaryCard user={profile} />
+      <Paper>
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton onClick={() => handleLogout()}>
+              <ListItemIcon>
+                <LogoutIcon />
+              </ListItemIcon>
+              <ListItemText primary="Log Out" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Paper>
+    </Stack>
+  );
 }
--- frontend/src/services/api-client.ts
+++ frontend/src/services/api-client.ts
@@ -4,7 +4,7 @@ import { refresh } from "./user-service";
 export { CanceledError };
 const apiClient = axios.create({
   // baseURL: process.env.SERVER_URL,
-  baseURL: 'http://10.100.102.10:3000/api',
+  baseURL: 'http://localhost:3000/api',
 });
 
 apiClient.interceptors.response.use(
